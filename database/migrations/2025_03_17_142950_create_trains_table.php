864cc6a339540ad64d94c2502603c27caab839a6
emazupp <zuppardo.emanuele2001@gmail.com>
2025-03-17 16:06:46 +0100

    ✅ done: fatto cose visto gente

---
+++ database/migrations/2025_03_17_142950_create_trains_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('trains', function (Blueprint $table) {
+            $table->id();
+            $table->string("society", 50);
+            $table->string("departure_station", 50);
+            $table->string("arrive_station", 50);
+            $table->dateTime("departure_time");
+            $table->dateTime("arrive_time");
+            $table->integer("carriage_total");
+
+            $table->boolean("on_time");
+            $table->boolean("is_cancelled");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('trains');
+    }
+};
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
+use App\Http\Controllers\TrainController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [TrainController::class, "index"]);
